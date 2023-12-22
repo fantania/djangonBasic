7044d4033d86e6f892190641af034e18a8c20e66
Fantine NYIRANDIKUBWIMANA <fantine@Fantines-MBP.lan>
2023-12-22 15:45:30 -0500

    last commit

--- mysite/employees/urls.py
+++ mysite/employees/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('<int:pk>', views.employees_detail) #the primary key -> pk can be any name
+    path('<int:pk>', views.employees_detail, name='employee_detail') #the primary key -> pk can be any name
 ]
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -24,5 +24,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('employees/', include('employees.urls')) #Forward this request to employees.urls
+    path('employee/', include('employees.urls')) #Redirect/Forward this request to employees.urls
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
